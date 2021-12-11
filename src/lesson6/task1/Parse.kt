fe778ac688ba4bc6d020b03e50f58f92be122f05
LeonidovDaniil <leonidoff.daniil@yandex.ru>
2021-12-12 00:39:17 +0300

    lesson 6 (2 tasks)

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -2,6 +2,8 @@
 
 package lesson6.task1
 
+import java.util.*
+
 // Урок 6: разбор строк, исключения
 // Максимальное количество баллов = 13
 // Рекомендуемое количество баллов = 11
@@ -138,7 +140,31 @@ fun bestHighJump(jumps: String): Int = TODO()
  * Вернуть значение выражения (6 для примера).
  * Про нарушении формата входной строки бросить исключение IllegalArgumentException
  */
-fun plusMinus(expression: String): Int = TODO()
+fun plusMinus(expression: String): Int {
+    val split = expression.split(" ")
+    for (i in split.indices) {
+        var counterForDigit = 0
+        var counterForSign = 0
+        for (j in split[i].indices) {
+            if (split[i][j].toString() == "+" || split[i][j].toString() == "-") counterForSign++
+            else counterForDigit++
+        }
+        if (counterForDigit == counterForSign) throw IllegalArgumentException() //не может содержаться знак и число одновременно
+    }
+    if (split[0] == "+" || split[0] == "-") throw IllegalArgumentException()
+    var res = split[0].toInt()
+    for (i in 0..split.size - 2) {
+        if (split[i] == split[i + 1] && (split[i] == "+" || split[i] == "-")) throw IllegalArgumentException() //Если повторяются знаки
+        if (split[i] != "+" && split[i] != "-" && split[i + 1] != "+" && split[i + 1] != "-") //Если повторяются цифры
+            throw IllegalArgumentException()
+    }
+    for (i in split.indices) {
+        if (split[i] == "+") res += split[i + 1].toInt()
+        if (split[i] == "-") res -= split[i + 1].toInt()
+    }
+    return res
+}
+
 
 /**
  * Сложная (6 баллов)
@@ -149,7 +175,16 @@ fun plusMinus(expression: String): Int = TODO()
  * Вернуть индекс начала первого повторяющегося слова, или -1, если повторов нет.
  * Пример: "Он пошёл в в школу" => результат 9 (индекс первого 'в')
  */
-fun firstDuplicateIndex(str: String): Int = TODO()
+fun firstDuplicateIndex(str: String): Int {
+    var idx = 0
+    val words = str.lowercase(Locale.getDefault()).split(" ")
+    if (words.size in 0..1) return -1
+    for (i in 0..words.size - 2) {
+        if (words[i] == words[i + 1]) return idx
+        idx += words[i].length + 1
+    }
+    return -1
+}
 
 /**
  * Сложная (6 баллов)
